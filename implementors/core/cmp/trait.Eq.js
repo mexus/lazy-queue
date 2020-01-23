903b7a1f710cb7d00bc8c30c7531e6236de85b29
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-01-23 21:18:40 +0000

    Deploy mexus/lazy-queue to github.com/mexus/lazy-queue.git:gh-pages

--- implementors/core/cmp/trait.Eq.js
+++ implementors/core/cmp/trait.Eq.js
@@ -1,9 +1,3 @@
 (function() {var implementors = {};
 implementors["futures"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"struct\" href=\"futures/sync/oneshot/struct.Canceled.html\" title=\"struct futures::sync::oneshot::Canceled\">Canceled</a>","synthetic":false,"types":["futures::sync::oneshot::Canceled"]},{"text":"impl&lt;T:&nbsp;<a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"struct\" href=\"futures/sync/mpsc/struct.SendError.html\" title=\"struct futures::sync::mpsc::SendError\">SendError</a>&lt;T&gt;","synthetic":false,"types":["futures::sync::mpsc::SendError"]},{"text":"impl&lt;T:&nbsp;<a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"struct\" href=\"futures/sync/mpsc/struct.TrySendError.html\" title=\"struct futures::sync::mpsc::TrySendError\">TrySendError</a>&lt;T&gt;","synthetic":false,"types":["futures::sync::mpsc::TrySendError"]}];
-
-            if (window.register_implementors) {
-                window.register_implementors(implementors);
-            } else {
-                window.pending_implementors = implementors;
-            }
-        })()
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- implementors/core/ops/deref/trait.DerefMut.js
+++ implementors/core/ops/deref/trait.DerefMut.js
@@ -1,10 +1,4 @@
 (function() {var implementors = {};
 implementors["futures"] = [{"text":"impl&lt;'a, T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.DerefMut.html\" title=\"trait core::ops::deref::DerefMut\">DerefMut</a> for <a class=\"struct\" href=\"futures/sync/struct.BiLockGuard.html\" title=\"struct futures::sync::BiLockGuard\">BiLockGuard</a>&lt;'a, T&gt;","synthetic":false,"types":["futures::sync::bilock::BiLockGuard"]},{"text":"impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.DerefMut.html\" title=\"trait core::ops::deref::DerefMut\">DerefMut</a> for <a class=\"struct\" href=\"futures/sync/struct.BiLockAcquired.html\" title=\"struct futures::sync::BiLockAcquired\">BiLockAcquired</a>&lt;T&gt;","synthetic":false,"types":["futures::sync::bilock::BiLockAcquired"]}];
 implementors["tokio_sync"] = [{"text":"impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.DerefMut.html\" title=\"trait core::ops::deref::DerefMut\">DerefMut</a> for <a class=\"struct\" href=\"tokio_sync/lock/struct.LockGuard.html\" title=\"struct tokio_sync::lock::LockGuard\">LockGuard</a>&lt;T&gt;","synthetic":false,"types":["tokio_sync::lock::LockGuard"]}];
-
-            if (window.register_implementors) {
-                window.register_implementors(implementors);
-            } else {
-                window.pending_implementors = implementors;
-            }
-        })()
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
